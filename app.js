3fbbbf82b57653e6faf2bc6b03936fa779eaae94
Sayed <sayed.sais@gmail.com>
2024-07-29 11:08:02 +0100

    completed MVP1

--- app.js
+++ app.js
@@ -14,6 +14,20 @@ const PORT = 3000;
 app.use(express.static("public"));
 app.use(express.json());
 
+app.get('/api/recipes', async (req, res) => {
+  try {
+  const showRecipes = await getRecipes()
+  res.status(200).json({
+    success: true, 
+    payload: showRecipes
+  })
+} catch (error) {
+  res.status(500).json({    
+    success: false, 
+    payload: 'Internal server error'})
+}
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
